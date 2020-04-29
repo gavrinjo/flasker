71b0a32dff8a9d268c4fb99ff5080345624cf6ac
gavrinjo <gavrinjo@gmx.com>
2020-04-29 23:03:44 +0200

    update to registration form

--- app/forms.py
+++ app/forms.py
@@ -1,5 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
+# from wtforms.fields.html5 import EmailField
 from wtforms.validators import ValidationError, DataRequired, EqualTo, Email
 from app.models import User
 
@@ -17,7 +18,7 @@ class RegistrationForm(FlaskForm):
     password = PasswordField("Password", validators=[DataRequired()])
     password2 = PasswordField("Repeat Password", validators=[
                               DataRequired(), EqualTo("password")])
-    submit = BooleanField("Regiser")
+    submit = SubmitField("Regiser")
 
     def validate_username(self, username):
         user = User.query.filter_by(username=username.data).first()
