159eb5b8b7e716870d73a462059659693b31edd8
gavrinjo <gavrinjo@gmx.com>
2020-04-29 15:30:57 +0200

    update

--- app/forms.py
+++ app/forms.py
@@ -22,9 +22,9 @@ class RegistrationForm(FlaskForm):
     def validate_username(self, username):
         user = User.query.filter_by(username=username.data).first()
         if user is not None:
-            raise ValidationError("username already used")
+            raise ValidationError("Please use a different username.")
 
     def validate_email(self, email):
         user = User.query.filter_by(email=email.data).first()
         if user is not None:
-            raise ValidationError('Please use a different email address.')
+            raise ValidationError("Please use a different email address.")
