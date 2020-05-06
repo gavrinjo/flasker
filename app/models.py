389cc458c4712924565d3d5d203005bac2fdb9c0
gavrinjo <drazen.gavric@gmail.com>
2020-05-06 11:24:55 +0200

    test commit

--- app/models.py
+++ app/models.py
@@ -5,6 +5,7 @@ from flask_login import UserMixin
 
 
 class User(UserMixin, db.Model):
+    
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
