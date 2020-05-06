f168804489434de44a13d090f80492a4e82e6c72
gavrinjo <drazen.gavric@gmail.com>
2020-05-06 12:07:51 +0200

    added avatars

--- app/models.py
+++ app/models.py
@@ -1,11 +1,11 @@
 from datetime import datetime
+from hashlib import md5
 from werkzeug.security import generate_password_hash, check_password_hash
 from app import db, login
 from flask_login import UserMixin
 
 
 class User(UserMixin, db.Model):
-    
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
@@ -22,6 +22,11 @@ class User(UserMixin, db.Model):
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode("utf-8")).hexdigest()
+        return f"https://www.gravatar.com/avatar/{digest}?d=identicon&r=x&s={size}"
+
 
 
 @login.user_loader
