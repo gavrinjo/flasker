935da539bcd889d2d5bf321044246d888a138cb9
gavrinjo <drazen.gavric@gmail.com>
2020-05-07 23:16:20 +0200

    facelift

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_avatars import Avatars
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -12,6 +13,7 @@ login = LoginManager(app)
 login.login_view = "login"
 migrate = Migrate(app, db)
 avatars = Avatars(app)
+bootstrap = Bootstrap(app)
 app.jinja_env.trim_blocks = True
 app.jinja_env.lstrip_blocks = True
 
--- config.py
+++ config.py
@@ -10,4 +10,4 @@ class Config(object):
     ) or "sqlite:///" + os.path.join(basedir, "app.db")
     SQLALCHEMY_TRACK_MODIFICATIONS = True
     AVATARS_SAVE_PATH = os.path.join(basedir, "static/avatars")
-    POSTS_PER_PAGE = 25
+    POSTS_PER_PAGE = 5
