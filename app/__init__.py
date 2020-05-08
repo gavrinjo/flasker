32521d04350addc1d06e511957de1d2ab1186402
gavrinjo <drazen.gavric@gmail.com>
2020-05-08 14:55:16 +0200

    facelift - WIP

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,7 @@ from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_avatars import Avatars
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -13,6 +14,7 @@ login = LoginManager(app)
 login.login_view = "login"
 migrate = Migrate(app, db)
 avatars = Avatars(app)
+moment = Moment(app)
 bootstrap = Bootstrap(app)
 app.jinja_env.trim_blocks = True
 app.jinja_env.lstrip_blocks = True
