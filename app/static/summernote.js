5cf4b508ce3fe328676ef5056017fe7b5c4c648d
gavrinjo <drazen.gavric@gmail.com>
2020-07-16 15:15:22 +0200

    WIP facelift

--- app/static/summernote.js
+++ app/static/summernote.js
@@ -1,6 +1,7 @@
 $('#summernote').summernote({
-    // height: 300,                 // set editor height
-    minHeight: 300,             // set minimum height of editor
-    // maxHeight: null,             // set maximum height of editor
-    // focus: true                  // set focus to editable area after initializing summernote
+    // height: 300, // set editor height
+    minHeight: 300, // set minimum height of editor
+    // maxHeight: null, // set maximum height of editor
+    //width: 150, //don't use px
+    // focus: true // set focus to editable area after initializing summernote
   });
